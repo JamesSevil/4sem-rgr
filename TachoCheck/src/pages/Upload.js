3178ee6c44c20a85c031c041a60582f3aaf5f204
JamesSevil <jsclassified24@gmail.com>
2025-05-18 04:24:12 +0700

    add upload files

--- TachoCheck/src/pages/Upload.js
+++ TachoCheck/src/pages/Upload.js
@@ -1,5 +1,58 @@
+import React, { useState } from "react";
+import { useNavigate } from "react-router-dom";
+import api from "../api";
+
 const Upload = () => {
-    
+    const [file, setFile] = useState(null);
+    const navigate = useNavigate();
+
+    const handleUpload = async (event) => {
+        event.preventDefault();
+
+        const formData = new FormData();
+        formData.append("file", file);
+        const username = sessionStorage.getItem("username") || localStorage.getItem("username");
+        formData.append("username", username);
+
+        try {
+            const response = await api.post("/upload", formData, {
+                headers: {
+                    "Content-Type": "multipart/form-data",
+                },
+            });
+
+            if (response.status === 200 && response.data.success) {
+                console.log(response.data.message);
+                const fileId = response.data.fileId;
+                navigate(`/details/${fileId}`);
+            } else {
+                console.error("Ошибка при загрузке файла: ", response.data.message);
+                alert(`Не удалось загрузить файл: ${response.data.message}`);
+            }
+        } catch (error) {
+            console.error("Ошибка при загрузке файла: ", error.response.data.message);
+            alert(`Не удалось загрузить файл: ${error.response.data.message}`);
+        }
+    };
+
+    return (
+        <div className="upload">
+            <h2>Загрузка файла</h2>
+
+            <form onSubmit={handleUpload}>
+                <div className="inputfile">
+                    <input
+                        type="file"
+                        onChange={(e) => setFile(e.target.files[0])}
+                        required
+                    />
+                    <button type="submit">Загрузить</button>
+                </div>
+            </form>
+
+            <p><button onClick={() => {navigate('/')}}>Назад</button></p>
+        </div>
+    );
 };
 
 export default Upload;
--- server/routes/upload.js
+++ server/routes/upload.js
@@ -1,7 +1,63 @@
 import express from "express";
+import multer from "multer";
+import path from "path";
+import Users from "../mongo/users.js";
+import Files from "../mongo/files.js";
+import verifytoken from "./verifytoken.js";
 
 const router = express.Router();
 
+// настройка multer для загрузки файлов в память
+const storage = multer.memoryStorage();
+const upload = multer({storage});
 
+function XOR(buffer, key) {
+    for (let i = 0; i < buffer.length; i++) {
+        buffer[i] ^= key;
+    }
+    return buffer;
+}
+
+router.post("/", upload.single("file"), verifytoken, async (req, res) => {
+    try {
+        const {username} = req.body;
+
+        if (!username) {
+            return res.status(400).json({success: false, message: "Не удалось распознать имя авторизованного пользователя!"});
+        }
+        if (!req.file) {
+            return res.status(400).json({success: false, message: "Файл не был загружен!"});
+        }
+        const extencion = path.extname(req.file.originalname).toLowerCase();
+        if (extencion != ".ddd") {
+            return res.status(415).json({success: false, message: "Неверный формат файла!\nМожно загружать только файлы, с расширением .ddd"});
+        }
+
+        const user = await Users.findOne({login: username});
+        if (!user) {
+            return res.status(404).json({success: false, message: "Пользователь не найден в базе данных!"});
+        }
+
+        const key = 123;
+        const decryptedBuffer = XOR(req.file.buffer, key);
+        const decryptedData = decryptedBuffer.toString("utf-8");
+
+        let jsonData;
+        try {
+            jsonData = JSON.parse(decryptedData);
+        } catch (parseError) {
+            // console.error("Ошибка парсинга JSON:", parseError);
+            return res.status(422).json({success: false, message: "Файл поврежден или не является валидным JSON!"});
+        }
+
+        const savedFile = new Files({...jsonData, fileName: req.file.originalname, uploadedAt: new Date(), owner: user._id});
+        await savedFile.save();
+
+        res.status(200).json({success: true, message: "Файл успешно загружен!", fileId: savedFile._id});
+    } catch (error) {
+        console.error("Ошибка при загрузке или расшифровке:", error);
+        res.status(500).json({success: false, message: "Ошибка сервера!"});
+    }
+});
 
 export default router;
